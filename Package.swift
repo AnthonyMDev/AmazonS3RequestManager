0c2e43ace45eed94e16e4f774e161acc26fbea2a
Matias Piipari <matias.piipari@gmail.com>
2021-09-05 22:49:51 +0300

    Updates CryptoSwift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "AmazonS3RequestManager",
-    platforms: [.macOS(.v10_15),
+    platforms: [.macOS(.v11),
                 .iOS(.v13)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -18,7 +18,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.5.0"),
         .package(url: "https://github.com/drmohundro/SWXMLHash.git", from: "4.2.0"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.3.1")
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.4.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
