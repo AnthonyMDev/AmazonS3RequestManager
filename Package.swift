24ca8435e4a3a18e7808ca0366bf5d57bf8dcd4e
Matias Piipari <matias.piipari@gmail.com>
2021-09-12 11:44:47 +0300

    10.15 as minimum again

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "AmazonS3RequestManager",
-    platforms: [.macOS(.v11),
+    platforms: [.macOS(.v10_15),
                 .iOS(.v13)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
